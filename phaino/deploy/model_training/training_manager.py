dad6e68e59af3aca76b846df2eb50b91573cbbd4
Vinicius Pires Matias Goncalves <vinicius.goncalves@ifood.com.br>
2021-12-28 08:11:00 -0300

    Training manager skeleton working

--- phaino/deploy/model_training/training_manager.py
+++ phaino/deploy/model_training/training_manager.py
@@ -1,24 +1,59 @@
-from multiprocessing import Process
+from multiprocessing import Process, Event, Queue
+import sys
+import time
 
 
 class TrainingManager():
     def __init__(self, models):
         self.models = models
 
+        self.models_indexed_by_priority = {}
+        for model in models:
+            self.models_indexed_by_priority[model["priority"]] = model
 
-    def handle_training(self, model):
+        self.process_map = {}
+        self.message_queue = Queue()
+        self.n_models = len(models)
+
+
+    def handle_training(self, model, priority, message_queue, training_data=None):
+        print("priority", priority)
         model.fit()
-        ##notify
+        message_queue.put(priority)
+        #TODO Switch to use the new model for inference at the main class
+
+
+    def __stop_lower_priority(self, priority):
+            stop_priorities = list(range(priority+1, self.n_models))
+
+            for priority in stop_priorities:
+                process = self.process_map[priority]
+                model_info = self.models_indexed_by_priority[priority]
+                if process.is_alive():
+                    print(f'Stopping process {process.name}, model_info: {model_info}')
+                    process.terminate()
 
     def adapt(self):
-        process_list = []
-        for model in self.models:
-            model_class = model['model']
-            #p = Process(target=model_class.fit, args=('bob',))
-            p = Process(target=self.handle_training, args=(model_class, ))
-            p.start()
-            process_list.append(p)
-            #p.join()
+        priorities = list(range(0, self.n_models))
 
-        for process in process_list:
-            process.join()
+        for priority in priorities:
+            model = self.models_indexed_by_priority[priority]
+            model_class = model['model']
+            p = Process(target=self.handle_training, args=(model_class, priority, self.message_queue,))
+            p.start()
+            self.process_map[priority] = p
+            time.sleep(0.05) # Avoid processes to be launched out of order
+
+    
+        while True:
+            priority = self.message_queue.get()
+            time.sleep(1)
+
+            print("Finished priority", priority)
+
+            if priority == 0:
+                print("Finishing training since the model with highest priority is ready")
+                sys.exit(0)
+            else:
+                self.__stop_lower_priority(priority)
+                
--- test/test_training_manager.py
+++ test/test_training_manager.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.case import skip
 from phaino.models.mock_model import MockModel
 from phaino.deploy.model_training.model_selection import assign_models_priority
 from phaino.deploy.model_training.training_manager import TrainingManager
@@ -49,35 +50,82 @@ class TestTrainingManager(unittest.TestCase):
                 "training_rate": 200,
                 "efectiveness": 30,
                 "inference_rate": 10,
-                "model":  MockModel()
+                "model":  MockModel(15)
             },
             {
-                "name": "model_3",
+                "name": "model_2",
                 "priority": 1,
                 "training_rate": 400,
                 "efectiveness": 20,
                 "inference_rate": 20,
-                "model":  MockModel()
+                "model":  MockModel(15)
             },
             {
-                "name": "model_2",
+                "name": "model_3",
                 "priority": 2,
                 "training_rate": 300,
                 "efectiveness": 20,
                 "inference_rate": 20,
-                "model":  MockModel()
+                "model":  MockModel(15)
             }
             
         ]
+
+
+        self.models_with_priority_different_times = [
+            {
+                "name": "model_1",
+                "priority": 0,
+                "training_rate": 200,
+                "efectiveness": 30,
+                "inference_rate": 10,
+                "model":  MockModel(10)
+            },
+            {
+                "name": "model_2",
+                "priority": 1,
+                "training_rate": 400,
+                "efectiveness": 20,
+                "inference_rate": 20,
+                "model":  MockModel(6)
+            },
+            {
+                "name": "model_3",
+                "priority": 2,
+                "training_rate": 300,
+                "efectiveness": 20,
+                "inference_rate": 20,
+                "model":  MockModel(60)
+            }
+            
+        ]
+
         print("setUpClass")
 
     def test_priority(self):
         models_with_priority = assign_models_priority(self.user_constraints, self.models)
         self.assertEqual(len(models_with_priority), 3)
 
+    @skip
     def test_adapt(self):
         training_manager = TrainingManager(self.models_with_priority)
-        training_manager.adapt()
+        with self.assertRaises(SystemExit) as cm:
+            training_manager.adapt()
+
+        self.assertEqual(cm.exception.code, 0)
+
+    #@skip
+    def test_adapt_different_times(self):
+        training_manager = TrainingManager(self.models_with_priority_different_times)
+
+        with self.assertRaises(SystemExit) as cm:
+            training_manager.adapt()
+
+        self.assertEqual(cm.exception.code, 0)
+
+
+        
+      
 
     @classmethod
     def tearDownClass(self):
